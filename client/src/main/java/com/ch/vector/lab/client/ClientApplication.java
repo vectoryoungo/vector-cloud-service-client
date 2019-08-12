7a5808dfb7405caca7b0c60bdf6e536b3ca332d5
居洪涛 <hongtao.ju@blockchain001.com>
2019-08-12 15:26:18 +0800

    add hystrix

--- client/src/main/java/com/ch/vector/lab/client/ClientApplication.java
+++ client/src/main/java/com/ch/vector/lab/client/ClientApplication.java
@@ -5,12 +5,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @EnableFeignClients
 @EnableDiscoveryClient
 @EnableCircuitBreaker
 @SpringBootApplication
+@EnableHystrix
 @EnableHystrixDashboard
 public class ClientApplication {
 
